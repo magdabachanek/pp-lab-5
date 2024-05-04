83264687118532008eaf0ee60c8732e1db3bdac8
magbach <161923809+magdabachanek@users.noreply.github.com>
2024-05-04 10:15:30 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(8, 4);
+        System.out.println("Wyniki: " + result);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("this is email message");
+        emailMessenger.sendMessage("Wyniki: " + result);
     }
 }
