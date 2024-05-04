f05061fd8e327d0d6c3a708d2c20f97181b63288
magbach <161923809+magdabachanek@users.noreply.github.com>
2024-05-04 09:50:09 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
-import company.beans.Person;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("Klaudia", 14);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-
-        try {
-            Person person = new Person("Piotr", 138);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("this is email message");
     }
 }
