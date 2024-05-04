5fcc806d2e9a9d93b772650b30425da0711abb26
magbach <161923809+magdabachanek@users.noreply.github.com>
2024-05-04 10:21:42 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(8, 4);
-        System.out.println("Wyniki: " + result);
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Anthony Hopkins", 58);
+            people[2] = new Person("Clint Estwood", 53);
+            people[3] = new Person("Tom Hanks", 39);
+            people[4] = new Person("Johnny Depp", 64);
+        } catch (InvalidAgeException e) {
+            System.out.println("Incorrect age: " + e.getMessage());
+        }
 
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Wyniki: " + result);
+
+        for (Person person : people) {
+            if (person != null) {
+                int value = MathUtils.add(person.getAge(), b);
+                emailMessenger.sendMessage(person.getName() + " value is: " + value);
+            }
+        }
     }
 }
